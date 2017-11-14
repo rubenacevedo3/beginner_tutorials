74786b5a00c5382a401cb1c7b8cc0a76fd2c8c1d
rubenacevedo3 <raceved3@umd.edu>
2017-11-13 19:34:26 -0800

    the changeBaseOutputString.cpp was changed to omit catkin errors for debuging purposes

--- src/changeBaseOutputString.cpp
+++ src/changeBaseOutputString.cpp
@@ -36,20 +36,20 @@
 
 bool changeS(const beginner_tutorials::changeBaseOutputString::Request  &req,
   const beginner_tutorials::changeBaseOutputString::Response &res) {
-  res.out = req.in;
-  ROS_INFO("request: s= ""%s", (std::string)req.in);
-  ROS_INFO("sending back response: [""%s\n""]", (std::string)res.out);
+  // res.out = req.in;
+  // ROS_INFO("request: s= ""%s", (std::string)req.in);
+  // ROS_INFO("sending back response: [""%s\n""]", (std::string)res.out);
   return true;
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "changeBaseOutputString");
-  ros::NodeHandle n;
+  // ros::init(argc, argv, "changeBaseOutputString");
+  // ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService
-    ("changeBaseOutputString", changeS);
-  ROS_INFO("Ready to change the string");
-  ros::spin();
+  // ros::ServiceServer service = n.advertiseService
+  //  ("changeBaseOutputString", changeS);
+  // ROS_INFO("Ready to change the string");
+  // ros::spin();
 
   return 0;
 }
